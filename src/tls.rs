85403e44b221cd10a7cf7844899c8cdb7176c106
K0R0VA <yarykov.m@edu.narfu.ru>
2021-08-30 22:04:45 +0300

    update certificate path

--- src/tls.rs
+++ src/tls.rs
@@ -4,8 +4,8 @@ use rustls::internal::pemfile::{certs, pkcs8_private_keys};
 pub fn load_ssl() -> ServerConfig {
     use std::io::BufReader;
 
-    const CERT: &'static [u8] = include_bytes!("../192.168.0.7.pem");
-    const KEY: &'static [u8] = include_bytes!("../192.168.0.7-key.pem");
+    const CERT: &'static [u8] = include_bytes!("../cert/192.168.0.7.pem");
+    const KEY: &'static [u8] = include_bytes!("../cert/192.168.0.7-key.pem");
 
     let mut cert = BufReader::new(CERT);
     let mut key = BufReader::new(KEY);
