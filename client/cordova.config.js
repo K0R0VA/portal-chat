27c1297240da612be743ae082493ed67c805d564
K0R0VA <yarykov.m@edu.narfu.ru>
2021-09-03 19:57:51 +0300

    cordova

---
+++ client/cordova.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    platforms: [
+        "ios",
+        "android"
+    ],
+    plugins: {
+        "cordova-plugin-device": {},
+        "cordova-plugin-inappbrowser": {},
+        "cordova-plugin-splashscreen": {},
+        "cordova-plugin-whitelist": {},
+        "cordova-plugin-ionic-webview": {},
+        "cordova-plugin-statusbar": {},
+        "cordova-plugin-ionic-keyboard": {}
+    }
+}
